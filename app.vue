ee9d0596c6e92fdab8c5bf21297bf83572450739
Pylyp Borysov <pborysov@nexuscognitive.com>
2023-02-16 17:05:18 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <button @click="hello">Hello</button>
+    <button @click="error">Error</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import LogRocket from 'logrocket';
+
+  LogRocket.init('fwiqiw/pylyp-test');
+
+  function hello(){
+    console.log('Hello')
+  }
+  function error(){
+    console.log(undefined()())
+  }
+</script>
+
+
+<style>
+  .container{
+    min-width: 100vw;
+    min-height: 100vh;
+    background-color: black;
+  }
+</style>
